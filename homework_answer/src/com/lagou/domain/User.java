1594957636f5a6e0243e292eb07ff121d54383ed
zimu <sunzhichao0713@163.com>
2020-12-28 21:09:50 +0800

    first update

---
+++ homework_answer/src/com/lagou/domain/User.java
@@ -0,0 +1,298 @@
+package com.lagou.domain;
+
+import java.util.Date;
+
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String sex;
+
+    private Date birthday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
+
+    public  void test2() {
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+             System.out.println(211111);
+             System.out.println(211111);
+             System.out.println(211111);
+             System.out.println(211111);
+             System.out.println(211111);
+        System.out.println(211111);
+    }
+
+   public void test6(){
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        }
+
+
+   public void test7(){
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        }
+   public void test8(){
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        }
+   public void test9(){
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        }
+   public void test6(){
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        }
+
+
+
+
+
+   public void test6(){
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        System.out.println(211111);
+        }
+
+}
